a49a3d3afc15bd933f57816328a459c380fea237
bee0511 <bee000092.cs10@nycu.edu.tw>
2025-07-16 13:25:19 +0800

    refactor: change file path for network

--- src/network.py
+++ src/network.py
@@ -4,7 +4,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 from utils import *
 import json
 
-def getNetworks(fabric, network_dir="networks", network_template_config_dir="network_templates", network_filter="", range=0):
+def getNetworks(fabric, network_dir="networks", network_template_config_dir="networks/network_templates", network_filter="", range=0):
     # range = show the networks from 0 to {range}
     url = getURL(f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/{fabric}/networks")
     headers = getAPIKeyHeader()
@@ -38,7 +38,7 @@ def getNetworks(fabric, network_dir="networks", network_template_config_dir="net
                 json.dump(network_template_config, f, indent=4)
                 print(f"Network config template for {network_name} (ID: {network_id}) is saved to {network_template_config_filename}")
 
-def getNetwork(fabric, network_name, network_dir="networks", network_template_config_dir="network_templates"):
+def getNetwork(fabric, network_name, network_dir="networks", network_template_config_dir="networks/network_templates"):
     url = getURL(f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/{fabric}/networks/{network_name}")
     headers = getAPIKeyHeader()
     r = requests.get(url, headers=headers, verify=False)
@@ -114,9 +114,9 @@ def deleteNetwork(fabric, network_name):
     print(f"Message: {r.text}")
 
 if __name__ == "__main__":
-    # getNetworks(fabric="Site1-Greenfield", network_dir="networks", network_template_config_dir="network_templates", network_filter="", range=10)
-    # getNetwork(fabric="Site1-Greenfield", network_name="bluenet1", network_dir="networks", network_template_config_dir="network_templates")
-    getNetworks(fabric="Site1-TSMC", network_dir="networks", network_template_config_dir="network_templates", network_filter="", range=0)
-    # createNetwork(filename="networks/Site1-TSMC_30000_bluenet1.json", network_template_config_file="network_templates/Site1-TSMC_30000_bluenet1.json")
-    # updateNetwork(filename="networks/Site1-TSMC_30000_bluenet1.json", network_template_config_file="network_templates/Site1-TSMC_30000_bluenet1.json")
+    # getNetworks(fabric="Site1-Greenfield", network_dir="networks", network_template_config_dir="networks/network_templates", network_filter="", range=10)
+    # getNetwork(fabric="Site1-Greenfield", network_name="bluenet1", network_dir="networks", network_template_config_dir="networks/network_templates")
+    # getNetworks(fabric="Site1-TSMC", network_dir="networks", network_template_config_dir="networks/network_templates", network_filter="", range=0)
+    # createNetwork(filename="networks/Site1-TSMC_30000_bluenet1.json", network_template_config_file="networks/network_templates/Site1-TSMC_30000_bluenet1.json")
+    updateNetwork(filename="networks/Site1-TSMC_30000_bluenet1.json", network_template_config_file="networks/network_templates/Site1-TSMC_30000_bluenet1.json")
     # deleteNetwork(fabric="Site1-TSMC", network_name="bluenet1")
