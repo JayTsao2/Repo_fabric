f60a7b8e5e236ef2be5568896db0ff4ff1ea981d
bee0511 <bee000092.cs10@nycu.edu.tw>
2025-07-17 10:25:02 +0800

    feat: get policy by ID

--- src/policy.py
+++ src/policy.py
@@ -4,7 +4,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 from utils import *
 import json
 
-def getPolicy(serial_number, policy_dir="policies"):
+def getPolicyBySerialNumber(serial_number, policy_dir="policies"):
     url = getURL(f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/policies/switches")
     headers = getAPIKeyHeader()
     params = {"serialNumber": serial_number}
@@ -18,10 +18,27 @@ def getPolicy(serial_number, policy_dir="policies"):
     for policy in policies:
         # Save each policy to a file
         policy_id = policy.get("policyId", "unknown")
-        filename = f"{policy_dir}/{serial_number}_{policy_id}.json"
+        filename = f"{policy_dir}/{policy_id}.json"
         with open(filename, "w") as f:
             json.dump(policy, f, indent=4)
             print(f"Policy config for {serial_number} (ID: {policy_id}) is saved to {filename}")
 
+def getPolicyByID(id, policy_dir="policies"):
+    url = getURL(f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/policies/{id}")
+    headers = getAPIKeyHeader()
+    r = requests.get(url, headers=headers, verify=False)
+    checkStatusCode(r)
+    if not os.path.exists(policy_dir):
+        os.makedirs(policy_dir)
+
+    policy = r.json()
+    policy_id = policy.get("policyId", "unknown")
+    # Save the policy to a file
+    filename = f"{policy_dir}/{policy_id}.json"
+    with open(filename, "w") as f:
+        json.dump(policy, f, indent=4)
+        print(f"Policy config for ID: {policy_id} is saved to {filename}")
+
 if __name__ == "__main__":
-    getPolicy("9LT3A74X1AS", "policies")
+    # getPolicyBySerialNumber("9LT3A74X1AS", "policies")
+    getPolicyByID("188510", "policies")
