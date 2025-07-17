29c98dff9b55b30a2d0c24c0d29ed9489afb4a34
bee0511 <bee000092.cs10@nycu.edu.tw>
2025-07-17 11:39:04 +0800

    feat: update and delete policy and save freeform configs

--- src/policy.py
+++ src/policy.py
@@ -4,6 +4,12 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 from utils import *
 import json
 
+def checkConfigExists(json_data):
+    # return True if the json data has a "nvPairs" key, and inside it has a "CONF" key
+    if "nvPairs" in json_data and "CONF" in json_data["nvPairs"]:
+        return True
+    return False
+
 def getPolicyBySerialNumber(serial_number, policy_dir="policies"):
     url = getURL(f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/policies/switches")
     headers = getAPIKeyHeader()
@@ -22,6 +28,15 @@ def getPolicyBySerialNumber(serial_number, policy_dir="policies"):
         with open(filename, "w") as f:
             json.dump(policy, f, indent=4)
             print(f"Policy config for {serial_number} (ID: {policy_id}) is saved to {filename}")
+        if checkConfigExists(policy):
+            if not os.path.exists(f"{policy_dir}/FreeForm"):
+                os.makedirs(f"{policy_dir}/FreeForm")
+            freeform_config = policy["nvPairs"]["CONF"]
+            template_name = policy.get("templateName", "")
+            freeform_filename = f"{policy_dir}/FreeForm/{policy_id}_{template_name}.sh"
+            with open(freeform_filename, "w") as ff:
+                ff.write(freeform_config)
+                print(f"Freeform config for {serial_number} (ID: {policy_id}) is saved to {freeform_filename}")
 
 def getPolicyByID(id, policy_dir="policies"):
     url = getURL(f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/policies/{id}")
@@ -39,6 +54,36 @@ def getPolicyByID(id, policy_dir="policies"):
         json.dump(policy, f, indent=4)
         print(f"Policy config for ID: {policy_id} is saved to {filename}")
 
+def updatePolicyByFile(filename):
+    headers = getAPIKeyHeader()
+    headers['Content-Type'] = 'application/json'
+    # Update a policy with the data from the file
+    try:
+        with open(filename, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print(f"File {filename} not found!")
+        exit()
+    except Exception as e:
+        print(f"Error: {e}")
+        exit()
+    payload = data
+    url = getURL(f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/policies/{id}")
+    r = requests.put(url, headers=headers, json=payload, verify=False)
+    checkStatusCode(r)
+    print(f"Status Code: {r.status_code}")
+    print(f"Message: {r.text}")
+
+def deletePolicy(id):
+    url = getURL(f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/policies/POLICY-{id}")
+    headers = getAPIKeyHeader()
+    r = requests.delete(url, headers=headers, verify=False)
+    checkStatusCode(r)
+    print(f"Status Code: {r.status_code}")
+    print(f"Message: {r.text}")
+
 if __name__ == "__main__":
-    # getPolicyBySerialNumber("9LT3A74X1AS", "policies")
-    getPolicyByID("188510", "policies")
+    getPolicyBySerialNumber("9LT3A74X1AS", "policies")
+    # getPolicyByID("54270", "policies")
+    # updatePolicyByFile("policies/POLICY-54260.json")
+    # deletePolicy("210860")
