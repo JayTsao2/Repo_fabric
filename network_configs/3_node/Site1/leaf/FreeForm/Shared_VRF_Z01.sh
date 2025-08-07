943102a25aef7bbcd8ef8574bd7b3ac562710953
bee0511 <bee000092.cs10@nycu.edu.tw>
2025-08-07 09:49:47 +0800

    chore: add hostname / fabric name

--- network_configs/3_node/Site1/leaf/FreeForm/Shared_VRF_Z01.sh
+++ network_configs/3_node/Site1/leaf/FreeForm/Shared_VRF_Z01.sh
@@ -2,4 +2,3 @@ router bgp $BGP_ASN
   vrf Z01
     neighbor Ethernet1/9-22
       inherit peer ebgp-peer-template-node
-
--- network_configs/3_node/Site3-Test/leaf/Freeform/VPC11.sh
+++ network_configs/3_node/Site3-Test/leaf/Freeform/VPC11.sh
@@ -1,11 +1,4 @@
 router bgp $BGP_ASN
-  template peer ebgp-peer-template-node
-    bfd
-    remote-as $BGP_ASN
-    address-family ipv4 unicast
-      as-override
-      disable-peer-as-check
-      soft-reconfiguration inbound always
   vrf vpc11
     neighbor Ethernet1/9-22
       inherit peer ebgp-peer-template-node
